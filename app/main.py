6e2c4aec5dde37be9f16ca68dc8343b0d54f34ed
Yomiyu Shambel <102787703+YomiyuShambel21@users.noreply.github.com>
2024-08-24 17:07:37 +0300

    update the data path

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
+import os
 import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from utils import load_data, perform_seasonal_decomposition, perform_box_plot_analysis, perform_correlation_analysis
 from statsmodels.tsa.seasonal import seasonal_decompose
 
 # Set the page title and description
@@ -20,32 +22,34 @@ st.sidebar.markdown("---")
 st.sidebar.header("Customization Options")
 selected_methodology = st.sidebar.selectbox("Select Methodology", ["Correlation Analysis", "Time-Series Analysis", "Box Plot Analysis"])
 
-# Perform statistical analysis based on the user selected methodology
+# Perform statistical analysis based on the user-selected methodology
 if uploaded_file is not None:
     # Load the data into a DataFrame
-    try:
-        # Read the CSV file
-        df_3 = pd.read_csv(uploaded_file)
-        
-        # Display the dataframe
+    df_3 = load_data(uploaded_file)
+    if df_3 is not None:
+        st.subheader("Uploaded file contents - Default Togo-Dapaong_QC data")
       
-        
-        # Perform further data processing or analysis if needed
-        
-    except pd.errors.ParserError:
+        with st.markdown(
+            f"<div style='max-height: 400px; overflow-y: scroll; overflow-x: scroll;'>",
+            unsafe_allow_html=True,
+        ):
+            st.dataframe(df_3.head(5000))
+        st.write(f"NOTE : Loading and Showing {min(5000, len(df_3))} rows out of {len(df_3)}") 
+    else:
         st.write("Error: Invalid CSV file. Please upload a valid CSV file.")
 else:
     # Use default CSV file if no file is uploaded
-    import os
-
-    # Get the directory of the current script
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    # Construct the path to the data file
-    file_path = os.path.join(current_dir, "..", "data", "togo-dapaong_qc.csv")
-
+    data_folder = os.path.join(os.path.dirname(__file__), 'data')
+    file_path = os.path.join(data_folder, 'cleaned_sierraleon_dataset.csv')
     df_3 = pd.read_csv(file_path)
-    st.subheader("Uploaded file contents - Default Serra-Lione Data")
-    st.dataframe(df_3)
+    st.markdown("<h4 style='font-size: 14px;'>Uploaded file contents - Default Cleaned Serra-Leone data</h4>", unsafe_allow_html=True)
+    with st.markdown(
+        f"<div style='max-height: 400px; overflow-y: scroll; overflow-x: scroll;'>",
+        unsafe_allow_html=True,
+    ):
+        st.dataframe(df_3.head(5000))
+    st.write(f"NOTE : Loading and Showing {min(5000, len(df_3))} rows out of {len(df_3)} ")
+
     # Methodology selection
     if selected_methodology == "Time-Series Analysis":
         # Seasonal Decomposition
